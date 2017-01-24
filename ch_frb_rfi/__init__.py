1055f766da4b5ce9dbd0293eb1c3d8ab12ef5788
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-01-23 20:29:37 -0500

    oops, half-finished comment

--- ch_frb_rfi/__init__.py
+++ ch_frb_rfi/__init__.py
@@ -18,5 +18,5 @@ For a list of all acquisitions available, see dir(ch_frb_rfi.acquisitions).
 # These functions are imported to the top level of the ch_frb_rfi package.
 from chains import clipper_chain, detrender_chain, transform_chain, new_transform
 
-# Syntax for accessing these is ch_
+# Syntax for accessing is e.g. ch_frb_rfi.acqusitions.toy()
 import acquisitions
