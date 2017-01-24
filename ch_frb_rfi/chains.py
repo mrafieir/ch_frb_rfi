02082488faedf1e50c06f923c6d9b25283bbbaf1
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-01-23 20:14:34 -0500

    add plot_nt optional argument to transforms

--- ch_frb_rfi/chains.py
+++ ch_frb_rfi/chains.py
@@ -12,11 +12,16 @@ class new_transform(rf_pipelines.py_wi_transform):
         pass
 
 
-def detrender_chain(ix, detrend_nt=2048, cpp=True):
-    return [ rf_pipelines.polynomial_detrender(deg=4, axis=1, nt_chunk=detrend_nt, cpp=cpp),
-             rf_pipelines.polynomial_detrender(deg=8, axis=0, nt_chunk=detrend_nt, cpp=cpp),
-             rf_pipelines.plotter_transform('dc_out%d' % ix , img_nfreq=512, img_nt=1200, downsample_nt=16) 
-           ]
+def detrender_chain(ix, detrend_nt=2048, cpp=True, plot_nt=16):
+    """If plot_nt=0, then plots will not be generated."""
+
+    transform_chain = [ rf_pipelines.polynomial_detrender(deg=4, axis=1, nt_chunk=detrend_nt, cpp=cpp),
+                        rf_pipelines.polynomial_detrender(deg=8, axis=0, nt_chunk=detrend_nt, cpp=cpp) ]
+
+    if plot_nt > 0:
+        transform_chain += [ rf_pipelines.plotter_transform('dc_out%d' % ix , img_nfreq=512, img_nt=1200, downsample_nt=plot_nt) ]
+
+    return transform_chain
 
 
 def clipper_chain(jx, ix, two_pass=True, clip_nt=1024, cpp=True):
@@ -32,14 +37,19 @@ def clipper_chain(jx, ix, two_pass=True, clip_nt=1024, cpp=True):
            ]
 
 
-def transform_chain(detrender_niter=2, clipper_niter=3, detrend_nt=2048, clip_nt=1024, two_pass=True, cpp=True):
-    transform_chain = [ rf_pipelines.plotter_transform('raw', img_nfreq=512, img_nt=1200, downsample_nt=16),
-                        rf_pipelines.badchannel_mask('/data/pathfinder/rfi_masks/rfi_20160705.dat', nt_chunk=clip_nt)
-                      ]
+def transform_chain(detrender_niter=2, clipper_niter=3, detrend_nt=2048, clip_nt=1024, two_pass=True, cpp=True, plot_nt=16):
+    """If plot_nt=0, then plots will not be generated."""
+
+    transform_chain = [ ]
+    
+    if plot_nt > 0:
+        transform_chain += [ rf_pipelines.plotter_transform('raw', img_nfreq=512, img_nt=1200, downsample_nt=plot_nt) ]
+
+    transform_chain += [ rf_pipelines.badchannel_mask('/data/pathfinder/rfi_masks/rfi_20160705.dat', nt_chunk=clip_nt) ]
 
     for ix in xrange(detrender_niter):
         for jx in xrange(clipper_niter):
             transform_chain += clipper_chain(jx, ix, two_pass=two_pass, clip_nt=clip_nt, cpp=cpp)
-        transform_chain += detrender_chain(ix, detrend_nt=detrend_nt, cpp=cpp)
+        transform_chain += detrender_chain(ix, detrend_nt=detrend_nt, cpp=cpp, plot_nt=plot_nt)
 
     return transform_chain
