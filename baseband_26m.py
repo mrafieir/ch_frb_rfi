e75bc6c4717539c4d71d14101f84e466b0098854
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2017-01-29 20:35:03 -0500

    update to include 1k 26m data

--- baseband_26m.py
+++ baseband_26m.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 import ch_frb_rfi
 
-s = ch_frb_rfi.acquisitions.baseband_26m_b1937_16_04_22()
+n = 40
+upc = False
 
-p = ch_frb_rfi.transform_parameters(plot_type = 'big',
-                                    bonsai_output_plot_stem='triggers')
-
-
+p = ch_frb_rfi.transform_parameters(plot_type = 'big', bonsai_output_plot_stem='triggers')
 t = ch_frb_rfi.transform_chain(p)
-t += [ ch_frb_rfi.bonsai.nfreq16K_3tree(p) ]
 
-s.run(t, outdir='baseband_26m_b1937_pipeline_outputs')
+if upc:
+    s = ch_frb_rfi.acquisitions.baseband_26m_b1937_16_04_22()
+    t += [ ch_frb_rfi.bonsai.nfreq16K_3tree(p) ]
+    dirname='16K_baseband_26m_b1937_pipeline_outputs'
+
+else:
+    s = ch_frb_rfi.acquisitions.baseband_26m_b1937_16_04_22_1K(n)
+    t += [ ch_frb_rfi.bonsai.nfreq1024_3tree(p) ]
+    dirname='1K_baseband_26m_b1937_pipeline_outputs'
+
+s.run(t, outdir=dirname)
