70882e0c5037e5aad22b404036f4c2640c3589e4
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2017-01-30 20:50:57 -0500

    enable web_viewer flag

--- baseband_26m.py
+++ baseband_26m.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 import ch_frb_rfi
 
-n = 40
-upc = False
+n = 20
+u16k = False
+web = True
 
-p = ch_frb_rfi.transform_parameters(plot_type = 'big', bonsai_output_plot_stem='triggers')
+plot_type = 'web_viewer' if web else 'big'
+
+p = ch_frb_rfi.transform_parameters(plot_type=plot_type, bonsai_output_plot_stem='triggers')
 t = ch_frb_rfi.transform_chain(p)
 
-if upc:
+if u16k:
     s = ch_frb_rfi.acquisitions.baseband_26m_b1937_16_04_22()
     t += [ ch_frb_rfi.bonsai.nfreq16K_3tree(p) ]
     dirname='16K_baseband_26m_b1937_pipeline_outputs'
@@ -17,4 +20,7 @@ else:
     t += [ ch_frb_rfi.bonsai.nfreq1024_3tree(p) ]
     dirname='1K_baseband_26m_b1937_pipeline_outputs'
 
-s.run(t, outdir=dirname)
+if not web:
+    s.run(t, outdir=dirname)
+else:
+    ch_frb_rfi.run_for_web_viewer(dirname, s, t)
