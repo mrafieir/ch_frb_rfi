3be20e18611280b40d6d91e18b7f2a07b55e842c
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2017-02-12 21:34:44 -0500

    cherry-pick from asym branch

--- baseband_26m.py
+++ baseband_26m.py
@@ -2,11 +2,11 @@
 import ch_frb_rfi
 
 # From the list below, select a sample index to process.
-sample = 5
+sample = 6
 # The first file index (starts at 0).
 start = 0 
 # The last file index (has no limit; must be greater than 'start').
-end = 400
+end = 40
 # Indicate whether to use the web_viewer as output.
 web = True
 
@@ -17,41 +17,55 @@ assert type(web) is bool
 # this (perhaps as a unified dictionary) in 'acquisitions.py' and 'bonsai.py'.
 # 'kfreq' is the upchannelization (from 1K=1024) factor. 'nypix' may be adjusted
 # so that each pixel (on waterfall plots) corresponds to a unique frequency channel.
+
+# No pulses
 if sample == 1:
     kfreq = 1
     path = '/data2/acqhack_b1937_1K/*.h5'
     bonsai_v = 1
-    nypix = 1024
+    nypix = 256
 
+# No pulses
 if sample == 2:
     kfreq = 1
     path = '/data2/baseband_26m_b1937_16_04_22/1k_B1937/*.h5'
     bonsai_v = 2
     nypix = 1024
 
+# No pulses; Wavy
 if sample == 3:
     kfreq = 16
     path = '/data2/baseband_26m_b1937_16_04_22/*.h5'
     bonsai_v = 1
     nypix = 16384
 
+# Wavy
 if sample == 4:
     kfreq = 128
     path = '/data2/baseband_26m_b1937_16_04_22/128k/*.h5'
     bonsai_v = 1
     nypix = 131072
 
+# Broken
 if sample == 5:
     kfreq = 16
     path = '/data2/baseband_26m_b1937_16_04_22/B0329_confirmed/*.h5'
     bonsai_v = 1
     nypix = 512
 
+# CHIME Pathfinder; Pulses
+if sample == 6:
+    kfreq = 1
+    path = '/data2/acqhack_confirmed_b0329_1K/*.h5'
+    bonsai_v = 1
+    nypix = 512
+
 # Define transform parameters. See 'ch_frb_rfi/chain.py' for a list of available parameters.
 p = ch_frb_rfi.transform_parameters(plot_type = 'web_viewer' if web else 'big', 
                                     bonsai_output_plot_stem = 'triggers', 
-                                    mask = [[730,760]],
-                                    clipper_niter = 2,
+                                    mask = [[730,760]] if sample != 6 else None,
+                                    maskpath = '/data/pathfinder/rfi_masks/rfi_20160705.dat' if sample == 6 else None,
+                                    clipper_niter = 4,
                                     detrender_niter = 2,
                                     plot_nypix = nypix,
                                     plot_nxpix = 1200,
@@ -69,7 +83,7 @@ s = ch_frb_rfi.acquisitions.sample(path, start, end)
 t += [ eval('ch_frb_rfi.bonsai.nfreq%sK_3tree(p, bonsai_v)' % kfreq) ]
 
 # Create a name for the output directory.
-dirname = '%sK_baseband_26m_d%d%dc' % (kfreq, p.detrender_niter, p.clipper_niter)
+dirname = '%sK-%dD-%dC' % (kfreq, p.detrender_niter, p.clipper_niter)
 
 # Run the pipeline (not) for web_viewer.
 if not web:
