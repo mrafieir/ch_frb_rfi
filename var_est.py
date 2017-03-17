fba409c7319472b76c7ca94235eaaab06e88adec
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2017-03-16 23:03:43 -0400

    add comment, var_est transform, a long acq

--- var_est.py
+++ var_est.py
@@ -3,15 +3,30 @@ import numpy as np
 import ch_frb_rfi
 import rf_pipelines
 
-s = ch_frb_rfi.acquisitions.sample('/data2/17-02-08-incoherent-data-avalanche/frb_incoherent_search_0/*.h5', 0, 2)
+test_bonsai = False
 
+# ~20 sec of data; good for catching bugs!
+s = ch_frb_rfi.acquisitions.sample('/data2/17-02-08-incoherent-data-avalanche/frb_incoherent_search_0/*.h5', 0, 1)
+
+# ~21 hours of data
+#s = ch_frb_rfi.acquisitions.incoherent_search1()
+
+# Define (mostly rfi-related) transform parameters
 p = ch_frb_rfi.transform_parameters(plot_type = 'web_viewer', 
                                     bonsai_output_plot_stem = 'triggers', 
                                     maskpath = '/data/pathfinder/rfi_masks/rfi_20160705.dat',
                                     rfi_level = 1,
                                     kfreq = 1)
 
+# Transform chain
 t = ch_frb_rfi.transform_chain(p)
-t += [ ch_frb_rfi.bonsai.nfreq1K_3tree(p, 1) ]
 
+# Append variance estimator to the chain
+t += [ rf_pipelines.variance_estimator(v1_chunk=128, v2_chunk=80, nt_chunk=1024, fname=None) ]
+
+# Append bonsai dedisperser
+if test_bonsai:
+    t += [ ch_frb_rfi.bonsai.nfreq1K_3tree(p, 1) ]
+
+# Run the pipeline for web viewer
 ch_frb_rfi.run_for_web_viewer('var_est', s, t)
