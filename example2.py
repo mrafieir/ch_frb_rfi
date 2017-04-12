c742020d73774838c7de7b3eb0a53f3f7c71b45e
Masoud Rafiei <mrafiei.ravandi@gmail.com>
2017-04-12 18:22:06 -0400

    temp: a small sample for testing L1Grouper

--- example2.py
+++ example2.py
@@ -14,6 +14,9 @@ import rf_pipelines
 # Let's choose an acquisition with a real pulsar!
 s = ch_frb_rfi.acquisitions.ex_pulsar_search1()
 
+# FIXME remove the next line before merging to master!
+s = rf_pipelines.chime_stream_from_times('/data2/17-02-08-incoherent-data-avalanche/frb_incoherent_search_1', 64945.1064525, 65000)
+
 # The following parameters are explained in 'ch_frb_rfi/chains.py'.
 (rfi_level, v1_chunk, v2_chunk) = (1, 32, 192)
 
