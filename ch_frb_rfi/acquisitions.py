3c4778512f06a399de5d0b860eebb08c85c7caf2
Masoud Rafiei Ravandi <mrafiei.ravandi@gmail.com>
2017-08-10 17:09:26 -0400

    cosmetics in docstring

--- ch_frb_rfi/acquisitions.py
+++ ch_frb_rfi/acquisitions.py
@@ -61,11 +61,15 @@ def crab_16k(n=800):
 
 def incoherent_pathfinder(path='/data2/17-02-08-incoherent-data-avalanche/', search_name, sample_index=None):
     """
-    The large catalog of incoherent-beam acquisitions (CHIME Pathfinder)
+    The large catalog of incoherent-beam acquisitions obtained by the CHIME Pathfinder (1K freq)
 
-    path : a full path to the directory of h5 files (str)
-    search_name : a search name (str)
-    sample_index : if None, then use the entire search. Otherwise, it must be an integer which corresponds to an interesting sample from the search!
+    Input arguments:
+
+    - path: a full path to the directory of h5 files (str)
+
+    - search_name: a search name (str)
+
+    - sample_index: if None, then use the entire search. Otherwise, it must be an integer which corresponds to an interesting sample from the input search.
     """
     
     assert (sample_index == None) or ((type(sample_index) == IntType) and sample_index >= 0)
