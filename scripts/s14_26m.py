65999b6f7e5d8ddfdd95cfbf9bd163fc48c836ae
Masoud Rafiei Ravandi <mrafiei.ravandi@gmail.com>
2017-09-13 18:50:58 -0400

    upgrade s14_26m

--- scripts/s14_26m.py
+++ scripts/s14_26m.py
@@ -8,26 +8,23 @@ s = ch_frb_rfi.sample('/data/ugiri/20170425T231531Z_chime_beamformed_1/*.h5', 0,
 
 p = ch_frb_rfi.transform_parameters(plot_type = 'web_viewer',
                                     maskpath = '/data/pathfinder/rfi_masks/rfi_20160705.dat',
-                                    make_plots = False,
+                                    make_plots = True,
                                     bonsai_output_plot_stem = 'triggers', 
                                     rfi_level = 2,
-                                    bonsai_use_analytic_normalization = False, 
-                                    var_est = True,
-                                    var_filename = './var_s14_26m.h5',
-                                    mask_filler_w_cutoff = 0.25,
+                                    spline = True,
+                                    bonsai_use_analytic_normalization = False,
+                                    bonsai_fill_rfi_mask = True,
+                                    var_est = False,
+                                    mask_filler = False,
                                     kfreq = 16,
                                     bonsai_dynamic_plotter = False,
+                                    bonsai_plot_all_trees = True,
+                                    L1Grouper_thr = 10,
                                     #plot_nypix = 16384
                                    )
 
 t = ch_frb_rfi.transform_chain(p)
-
-ch_frb_rfi.run_in_scratch_dir('s14_26m', s, t)
-
-p.var_est = False; p.mask_filler = True; p.make_plots = True
-
-t = ch_frb_rfi.transform_chain(p)
-t += [ ch_frb_rfi.bonsai.nfreq16K_3tree(p, 1) ]
+t += [ ch_frb_rfi.bonsai.nfreq16K_7tree(p, 1) ]
 
 ch_frb_rfi.run_for_web_viewer('s14_26m', s, t)
 
