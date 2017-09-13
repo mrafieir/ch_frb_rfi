7a8ffa1e52f3f391b6c0b4aa19bda4265eb3798e
Masoud Rafiei Ravandi <mrafiei.ravandi@gmail.com>
2017-09-13 13:58:58 -0400

    upgrade s11, s12 (fix acq), s13 (fix acq)

--- scripts/s11.py
+++ scripts/s11.py
@@ -10,16 +10,18 @@ import rf_pipelines
 s = ch_frb_rfi.acquisitions.incoherent_pathfinder(search_name='frb_incoherent_search_2')
 
 p = ch_frb_rfi.transform_parameters(plot_type = 'web_viewer',
-                                    make_plots = False,
+                                    make_plots = True,
                                     bonsai_output_plot_stem = 'triggers',
-                                    rfi_level = 2,
                                     maskpath = '/data/pathfinder/rfi_masks/rfi_20160705.dat',
+                                    detrender_niter = 2,
+                                    clipper_niter = 6,
+                                    spline = True,
                                     bonsai_use_analytic_normalization = False,
                                     bonsai_hdf5_output_filename = None,
                                     bonsai_nt_per_hdf5_file = None,
-                                    var_est = True,
+                                    bonsai_fill_rfi_mask = True,
+                                    var_est = False,
                                     mask_filler = False,
-                                    var_filename = './var_s11.h5',
                                     mask_filler_w_cutoff = 0.5,
                                     bonsai_plot_threshold1 = 7,
                                     bonsai_plot_threshold2 = 10,
@@ -29,13 +31,7 @@ p = ch_frb_rfi.transform_parameters(plot_type = 'web_viewer',
                                     bonsai_event_outfile = 'events_s11')
 
 t = ch_frb_rfi.transform_chain(p)
-
-ch_frb_rfi.run_in_scratch_dir('s11', s, t)
-
-(p.var_est, p.mask_filler, p.make_plots) = (False, True, True)
-
-t = ch_frb_rfi.transform_chain(p)
-t += [ ch_frb_rfi.bonsai.nfreq1K_7tree(p, 1) ]
+t += [ ch_frb_rfi.bonsai.nfreq1K_7tree(p, v=3) ]
 
 ch_frb_rfi.run_for_web_viewer('s11', s, t)
 
--- scripts/s12.py
+++ scripts/s12.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 """
-2 hours of data
+5 min of data
 """
 
 import numpy as np
 import ch_frb_rfi
 import rf_pipelines
 
-s = ch_frb_rfi.acquisitions.incoherent_pathfinder(search_name='frb_incoherent_search_3', sample_index=7)
+s = ch_frb_rfi.acquisitions.incoherent_pathfinder(search_name='frb_incoherent_search_3', sample_index=6)
 
 p = ch_frb_rfi.transform_parameters(plot_type = 'web_viewer',
-                                    make_plots = False,
+                                    make_plots = True,
                                     bonsai_output_plot_stem = 'triggers',
-                                    rfi_level = 2,
                                     maskpath = '/data/pathfinder/rfi_masks/rfi_20160705.dat',
+                                    rfi_level = 2,
+                                    spline = True,
                                     bonsai_use_analytic_normalization = False,
                                     bonsai_hdf5_output_filename = None,
                                     bonsai_nt_per_hdf5_file = None,
-                                    var_est = True,
+                                    bonsai_fill_rfi_mask = True,
+                                    var_est = False,
                                     mask_filler = False,
-                                    var_filename = './var_s12.h5',
                                     mask_filler_w_cutoff = 0.5,
                                     bonsai_plot_threshold1 = 7,
                                     bonsai_plot_threshold2 = 10,
@@ -29,13 +30,7 @@ p = ch_frb_rfi.transform_parameters(plot_type = 'web_viewer',
                                     bonsai_event_outfile = 'events_s12')
 
 t = ch_frb_rfi.transform_chain(p)
-
-ch_frb_rfi.run_in_scratch_dir('s12', s, t)
-
-(p.var_est, p.mask_filler, p.make_plots) = (False, True, True)
-
-t = ch_frb_rfi.transform_chain(p)
-t += [ ch_frb_rfi.bonsai.nfreq1K_7tree(p, 1) ]
+t += [ ch_frb_rfi.bonsai.nfreq1K_7tree(p, v=3) ]
 
 ch_frb_rfi.run_for_web_viewer('s12', s, t)
 
