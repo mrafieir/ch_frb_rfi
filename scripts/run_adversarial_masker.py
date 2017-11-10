5476d0f89b9e9382ba9ae7aa51f19d1c9d13d3f6
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-11-10 18:11:51 -0500

    update run_adversarial_masker.py

--- scripts/run_adversarial_masker.py
+++ scripts/run_adversarial_masker.py
@@ -2,9 +2,10 @@
 #
 # This script is intended to be run on frb1.physics.mcgill.ca.
 #
-# The 'adversarial_masker' is a transform intended to stress-test the online variance
-# estimation logic by masking regions of the input array. If the online variance 
-# estimation logic is working well, then the  trigger plots shouldn't show any false positives!
+# The 'adversarial_masker' is a half-finished transform, intended to stress-test the
+# online variance estimation logic by masking a bunch of rectangle-shaped regions of 
+# the input array.  If the online variance estimation logic is working well, then the 
+# trigger plots shouldn't show any false positives!
 #
 # This script runs the adversarial_masker and creates an 'adversarial_masker'
 # web viewer entry.
@@ -14,11 +15,9 @@ import numpy.random
 import rf_pipelines
 import ch_frb_rfi
 
-# Total size of timestream, currently set by hand.
-# If the adversarial_masker code is extended to include more rectangles,
-# nt_tot will probably need to be increased!
-nt_tot = 700 * 1024 * 6
-    
+bonsai_config_filename = '/data/bonsai_configs/bonsai_nfreq1024_7tree_v4.hdf5'
+nt_tot = 8192 * 1024
+n_zoom = 8
 
 s = rf_pipelines.gaussian_noise_stream(nfreq=1024, 
                                        nt_tot = nt_tot,
@@ -32,14 +31,15 @@ t_plotter = rf_pipelines.plotter_transform(img_prefix='waterfall1',
                                            img_nfreq = 256,
                                            img_nt = 256,
                                            downsample_nt = 16,
-                                           n_zoom = 6)
+                                           n_zoom = n_zoom)
 
-t_dedisp = rf_pipelines.bonsai_dedisperser(config_filename = '/data/bonsai_configs/bonsai_nfreq1024_7tree_v1.txt',
+t_dedisp = rf_pipelines.bonsai_dedisperser(config_filename = bonsai_config_filename,
+                                           fill_rfi_mask = True,
                                            img_prefix = 'toy_pipeline',
                                            img_ndm = 256,
                                            img_nt = 256,
                                            downsample_nt = 16,
-                                           n_zoom = 6,
+                                           n_zoom = n_zoom,
                                            plot_all_trees = True)
 
-ch_frb_rfi.run_for_web_viewer('adversarial_masker', s, [t_masker,t_plotter,t_dedisp])
+ch_frb_rfi.run_for_web_viewer('adversarial_masker', s, [t_masker, t_plotter, t_dedisp])
