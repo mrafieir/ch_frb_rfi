fc06a99e8684f6ade7d92e89bc3dae243f372e3b
chitrangpatel <chitrang.patel@yahoo.com>
2018-03-14 07:05:28 -0700

    Replay with L1b.

--- replay_with_L1b.py
+++ replay_with_L1b.py
@@ -11,11 +11,12 @@ import numpy as np
 import ch_frb_rfi
 import rf_pipelines
 from glob import glob
+from ch_frb_rfi.transforms import write_mask
 
 # Let's choose an acquisition with a real pulsar!
 filename_list = glob("/frb-archiver-1/acq_data/frb_run_9_night 3/beam_0139/chunk*.msg")
 filename_list.sort()
-s = rf_pipelines.chime_frb_stream_from_filename_list(filename_list[450:550], nt_chunk=1024, noise_source_align= 0)
+s = rf_pipelines.chime_frb_stream_from_filename_list(filename_list[150:600], nt_chunk=1024, noise_source_align= 0)
 
 # The following parameters are explained in 'ch_frb_rfi/chains.py'.
 (rfi_level, v1_chunk, v2_chunk) = (0, 32, 192)
@@ -35,24 +36,28 @@ s = rf_pipelines.chime_frb_stream_from_filename_list(filename_list[450:550], nt_
 p = ch_frb_rfi.transform_parameters(plot_type = 'web_viewer',
                                     make_plots = False,
                                     #bonsai_output_plot_stem = 'triggers', 
-                                    maskpath = '/data/pathfinder/rfi_masks/rfi_20160705.dat',
-                                    rfi_level = rfi_level,
+                                    #maskpath = None,
+                                    #maskpath = '/data/pathfinder/rfi_masks/rfi_20160705.dat', # Bad Channel mask
+                                    #detrender_niter = 2, # For current chain of cleaning in L1
+                                    #clipper_niter = 6, # For current chain of cleaning in L1
+                                    #spline = True, # For current chain of cleaning in L1
+                                    #rfi_level = rfi_level, # Uncomment this to do a minimal RFI cleaning.
                                     bonsai_use_analytic_normalization = False, 
                                     bonsai_hdf5_output_filename = None,
                                     bonsai_nt_per_hdf5_file = None,
+                                    #bonsai_fill_rfi_mask = True,
                                     var_est = False,
-                                    mask_filler = False,
-                                    #var_filename = './var_example2.h5',
-                                    variance_estimator_v1_chunk = v1_chunk,
-                                    variance_estimator_v2_chunk = v2_chunk,
-                                    mask_filler_w_cutoff = 0.5,
+                                    #mask_filler = True,
+                                    #variance_estimator_v1_chunk = v1_chunk,
+                                    #variance_estimator_v2_chunk = v2_chunk,
+                                    #mask_filler_w_cutoff = 0.5,
                                     bonsai_plot_threshold1 = 7,
                                     bonsai_plot_threshold2 = 10,
                                     bonsai_dynamic_plotter = False,
                                     L1b_config = 'L1b_config_site.yaml')
 
 # Using the specified parameters make a chain of transforms for estimating the variance.
-#t = ch_frb_rfi.transform_chain(p)
+t16k = ch_frb_rfi.transform_chain(p)
 
 # Combine stream and transforms into a pipeline.
 #pipeline = rf_pipelines.pipeline([s]+t)
@@ -70,11 +75,22 @@ p = ch_frb_rfi.transform_parameters(plot_type = 'web_viewer',
 #p.mask_filler = False
 #p.make_plots = True
 
-t = ch_frb_rfi.transform_chain(p)
-t += [ ch_frb_rfi.bonsai.make_dedisperser(p, '/home/l1operator/ch_frb_l1/bonsai_configs/bonsai_production_noups_nbeta1_v2_nbits32.hdf5') ]
-#t += [ ch_frb_rfi.bonsai.make_dedisperser(p, '/data/bonsai_configs/bonsai_production_noups_nbeta1_v2.hdf5') ]
-#t += [ ch_frb_rfi.bonsai.nfreq1K_3tree(p, fpga_counts_per_sample=512, v=1) ]
-pipeline = rf_pipelines.pipeline([s]+t)
+####### Run L1 with default chain of  RFI  Cleaning########
+
+#t1k = ch_frb_rfi.transform_chain(p)
+#p1k = rf_pipelines.pipeline(t1k)
+
+#p.detrend_last = False
+#_t1k = ch_frb_rfi.transform_chain(p)
+#_p1k = rf_pipelines.pipeline(_t1k)
+#t16k = [ rf_pipelines.wi_sub_pipeline(_p1k, nfreq_out=1024, nds_out=1) ]
+#t16k += ch_frb_rfi.chains.detrender_chain(p, ix=0)
+
+###### Run Bonsai #############
+t16k += [ch_frb_rfi.bonsai.make_dedisperser(p, '/home/l1operator/ch_frb_l1/bonsai_configs/bonsai_production_ups_nbeta1_v2_nbits32.hdf5')] # 32 bits
+#t16k += [ ch_frb_rfi.bonsai.make_dedisperser(p, '/data/bonsai_configs/bonsai_production_ups_nbeta1_v2.hdf5') ] # 16 bits
+#t16k += [write_mask.WriteWeights(basename = 'frb_run_9_beam_139_no_rfi_cleaning')]
+pipeline = rf_pipelines.pipeline([s]+t16k)
 
 # Second pipeline run: we use the wrapper function run_for_web_viewer().
 # Run the pipeline (again) but now with the mask_filler and bonsai dedisperser.
